c016575e0bc1c17e18a671f6284079543dc5a3dd
filipeguimel <guimelfilipe@hotmail.com>
2021-12-06 11:32:53 -0300

    create feature Nossos Parceiros front

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Logo } from '../src/assets'
 import './style.css';
-
+import NossosParceiros from './Views/Nossos Parceiros/NossosParceiros';
 const App = () => {
   return (
     <div className="App">
-
+      NossosParceiros
       <img src={Logo} alt="" />
       <p>
         Made with
---
+++ src/Views/Nossos Parceiros/NossosParceiros.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const NossosParceiros = () => {
+    return (
+        <div class="NossosParceiros">
+            <p class="subtitle">
+                "Marcas que apoiam nossos projeto."
+            </p>
+            <div class= "logos">
+                <img src='' alt="logo da Novo Jeito"/>
+                <img src='' alt="logo da Transforma Brasil"/>
+                <img src='' alt="logo da Transforma Recife"/>
+                <img src='' alt="logo da RD"/>
+                <img src='' alt="logo da Creative Studio"/>
+                <img src='' alt="logo da Neoenergia"/>
+                <img src='' alt="logo da Lima Bezerra"/>
+            </div>
+        </div>
+  )
+    }
+  
+  export default NossosParceiros;
+  
