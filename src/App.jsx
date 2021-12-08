228033de661a088984457b7f00ed8b50daf77f39
JLucasM <menezes.joaolucas@gmail.com>
2021-12-08 18:20:26 -0300

    add banner archives

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './style.css';
 import NossosParceiros from './Views/Nossos Parceiros/NossosParceiros';
 import {Donate} from './Views/doacao/doacao';
+import {Banner} from './Views/Banner/Banner';
 
 const App = () => {
   return (
     <div className="App">
+      <Banner/>
       <Donate/>
+
   </div>
 )
   }
---
+++ src/Views/Banner/Banner.jsx
@@ -0,0 +1,47 @@
+import foto from '../../assets/foto.jpeg'
+import {ReactComponent as IconSVG} from '../../assets/a.svg'
+import './style.css'
+import {mainUrl} from '../../assets/index'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import {ReactComponent as SetaSVG} from '../../assets/seta.svg'
+
+
+
+export const Donate = () => {
+    const [text, setText] = useState('');
+    const [image, setImage] = useState('');
+    const [pix, setPix] = useState('');
+
+    const getInfos = async () => {
+      const res = await axios.get(`${mainUrl}/deseja-nos-ajudars`);
+      const { descricao, imagem, pix } = res.data[0];
+      setText(descricao);
+      setImage(imagem[0].url)
+      setPix(pix);
+    };
+      useEffect(() => {
+        getInfos();
+      }, []);
+
+    
+        return(
+           <div className='container'>
+                <div className='card'>
+                < IconSVG className='svg'/>
+                <img src={image} className='image'/>
+                    <h1>Deseja nos ajudar?</h1>
+                    <p>{text}</p>
+                    
+        <button type="button" className='doacao'><a href='https://escolacriarerecriar.com/ccb_donate' className='link'>            
+            Faça uma doação</a>
+            <SetaSVG className='seta'/>
+            </button>
+
+        <p className='pix'> PIX: {pix}</p>
+                    
+                </div>
+            </div>
+        )
+    }
+    export default Donate;
