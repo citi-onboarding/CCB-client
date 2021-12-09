48fd0b8c249da078789580cc571166d1165c5456
filipeguimel <guimelfilipe@hotmail.com>
2021-12-09 10:34:13 -0300

    create a secsion Contato

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import './style.css';
+import Contato from './Views/Contato/Contato';
 import NossosParceiros from './Views/Nossos Parceiros/NossosParceiros';
 const App = () => {
   return (
     <div className="App">
       <NossosParceiros/>
-      
+      <Contato/>
       
   </div>
 )
